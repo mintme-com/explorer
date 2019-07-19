0263a0dcfd14a253e4c1337e194f70a26d455536
lumat <lukasz1235@gmail.com>
2019-07-19 15:18:43 +0200

    Fix extra data decoding

--- routes/filters.js
+++ routes/filters.js
@@ -98,7 +98,7 @@ var hex2ascii = function (hexIn) {
     }
     str = baToString(ba);
   } catch (e) {
-    for (let i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    for (let i = 2; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
   }
   return str;
 };
